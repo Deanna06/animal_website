924cb14ae1406128a33063ec0d3272ea53e1b18b
LCAdmin5 <lcadmin5@LCAdmin5s-Air.fios-router.home>
2016-05-09 16:29:47 -0400

    first command

---
+++ app.js
@@ -0,0 +1,54 @@
+
+var lion = "Rawrrr";
+
+console.log(rawrrr);
+
+alert(lion);
+
+
+
+
+
+
+// if (1 < 10);{
+// 	console.log("this is true");
+// }
+// else{
+// 	console.log("this is false");
+// }
+
+// if (10 < 20){
+// 	console.log("This is true");
+// }
+// else{
+// 	console.log("this is false");
+// }
+
+// arrays
+
+// var colors = ["red", "pink", "blue", "black", "white"];
+// console.log(colors[1]);
+
+// // var favoriteNumbers = [7, 1, 2, 5, 17, 6, 0];
+// // console.log(favoriteNumbers[4]);
+// // multidimesional arrays
+// var snack =[colors, favoriteNumbers ];
+
+// LOOPS!
+// console.log(colors.length);
+
+ // for(var i = 0; <= colors.length - 1; i++) {
+ 	// console.log(i + " " +colors[i])
+ }
+
+
+
+
+
+
+
+
+
+
+
+
